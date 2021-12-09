835626a773106f2dbe4f685e35f75dcd84991f36
phannhattan1062001 <73206189+phannhattan1062001@users.noreply.github.com>
2021-12-09 09:44:49 +0700

    First commit

---
+++ src/main/java/com/abc/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.abc.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.servlet.ModelAndView;
+
+ 
+
+@Controller
+
+public class HomeController {
+
+ 
+
+       @RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+
+       public ModelAndView login() {
+
+              ModelAndView mav = new ModelAndView("home");
+
+              return mav;
+
+       }
+
+}
